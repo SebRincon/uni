15c29fc2ed93b485f2d4405435431c5c6f59d98d
seb4594 <65371156+seb4594@users.noreply.github.com>
2025-09-20 02:19:39 -0500

    backend fix

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,12 +3,9 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'twitter-clone-media',
   access: (allow) => ({
-    'media/*': [
-      allow.guest.to(['read']), // Allow everyone to read all media files
-      allow.authenticated.to(['read']) // Authenticated users can also read all files
-    ],
     'media/{entity_id}/*': [
-      allow.entity('identity').to(['write', 'delete']) // Users can only write/delete their own files
+      allow.guest.to(['read']), // Allow everyone to read all files
+      allow.authenticated.to(['read', 'write', 'delete']) // Authenticated users have full access
     ],
   })
 });
